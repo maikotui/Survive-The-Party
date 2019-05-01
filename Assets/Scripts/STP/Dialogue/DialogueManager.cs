9e2603a17fb50646dd018673bb262cbb3e0c4918
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2019-05-01 14:59:47 -0600

    [ADD] Added DialogueManager, haven't implemented yet.

---
+++ Assets/Scripts/STP/Dialogue/DialogueManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace STP.Dialogue
+{
+    public class DialogueManager : Singleton<DialogueManager>
+    {
+        
+    }
+}
